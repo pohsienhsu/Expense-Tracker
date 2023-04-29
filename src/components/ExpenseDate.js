775130f67a1b9f32aaff729ae13e53f8f06b657b
phsu40 <phsu40@gatech.edu>
2023-04-28 20:23:43 -0700

    Create Expense component and Card component to serve as container and default styling

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,5 +1,6 @@
-const ExpenseDate = (props) => {
+import './ExpenseDate.css';
 
+function ExpenseDate(props) {
   const month = props.date.toLocaleString('en-US', { month: 'long' });
   const day = props.date.toLocaleString('en-US', { day: '2-digit' });
   const year = props.date.getFullYear();
@@ -7,10 +8,10 @@ const ExpenseDate = (props) => {
   return (
     <div className="expense-date">
       <div className="expense-date__month">{month}</div>
-      <div className="expense-date__day">{day}</div>
       <div className="expense-date__year">{year}</div>
+      <div className="expense-date__day">{day}</div>
     </div>
-  )
+  );
 }
 
 export default ExpenseDate;
