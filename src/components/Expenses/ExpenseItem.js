3c295436396e8d61e338a87811d961cc928dd588
phsu40 <phsu40@gatech.edu>
2023-04-28 21:40:06 -0700

    Add click handler to button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -6,6 +6,11 @@ import './ExpenseItem.css'
 
 
 const ExpenseItem = (props) => {
+
+  const clickHandler = () => {
+    console.log('clicked');
+  }
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -13,6 +18,7 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   )
 }
